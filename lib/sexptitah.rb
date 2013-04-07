0b0736413bc10c7f9cfd8b3d89cd8a7b665123c2
Alessandro Piras <laynor@gmail.com>
2013-04-07 03:35:52 +0200

    Refined serialization, working deserialization.

--- lib/sexptitah.rb
+++ lib/sexptitah.rb
@@ -1,6 +1,8 @@
 require 'sexptitah/version'
 require 'sexptitah/list'
 
+#####################
+## sexp serialization
 class Object
   def to_sexp
     inspect
@@ -55,4 +57,106 @@ class Hash
   def to_sexp
     to_list.to_sexp
   end
+end
+
+class FalseClass
+  def to_sexp
+    'nil'
+  end
+end
+
+######################################
+## sexp deserialization
+##
+## lists -> Cons
+## Arrays -> Array
+##
+## Parsing code adapted from sexpistol
+
+require 'strscan'
+
+class SexptitahParser < StringScanner
+
+  def initialize(string)
+    unless(string.count('(') == string.count(')'))
+      raise Exception, "Missing closing parentheses"
+    end
+    super(string)
+  end
+
+  @@matching_paren = {
+    '(' => ')',
+    '{' => '}',
+    '[' => ']'
+  }
+
+  # expected: the expected closing paren or delimiter
+
+  def parse(level=0, expected=nil, list=false)
+    chain = false
+    exp = []
+    while true
+      case fetch_token
+      when '('
+        exp << parse(level + 1, @@matching_paren[@token], true)
+      when '[', '{'
+        exp << parse(level + 1, @@matching_paren[@token])
+      when ')', ']', '}'
+        raise RuntimeError.new("Mismatched closing paren")  if @token != expected
+        break
+      when :"'"
+        case fetch_token
+        when '(', '[', '{' then exp << [:quote].concat([parse(level+1, @@matching_paren[@token])])
+        else exp << [:quote, @token]
+        end
+      when :'.'
+        if list
+          rest = parse(level, ')')
+          raise RuntimeError.new('Syntax error - more than one sexp after dot.')  if rest.length > 1
+          exp = exp + rest
+          chain = true
+        else
+          raise RuntimeError.new('Syntax error - dot outside of a list.')
+        end
+      when :nil
+        exp << nil
+      when String, Fixnum, Bignum, Float, Symbol
+        exp << @token
+      when nil
+        break
+      end
+    end
+    list ? exp.send(chain ? :to_cons_chain : :to_list) : exp
+  end
+
+  def fetch_token
+    skip(/\s+/)
+    return nil if(eos?)
+
+    @token =
+    # Match parentheses
+    if scan(/[\[\{\(\)\}\]]/)
+      matched
+    # Match a string literal
+    elsif scan(/"([^"\\]|\\.)*"/)
+      eval(matched)
+    # Match a float literal
+    elsif scan(/[\-\+]? [0-9]+ ((e[0-9]+) | (\.[0-9]+(e[0-9]+)?))/x)
+      matched.to_f
+    # Match an integer literal
+    elsif scan(/[\-\+]?[0-9]+/)
+      matched.to_i
+    # Match a comma (for comma quoting)
+    elsif scan(/'/)
+      matched.to_sym
+    # Match a symbol
+    elsif scan(/((\\\s)|[^\s\(\)]|)+/)
+      matched.gsub('-', '_').to_sym
+    # If we've gotten here then we have an invalid token
+    else
+      near = scan %r{.{0,20}}
+      raise "Invalid character at position #{pos} near '#{near}'."
+    end
+  end
+
 end
--- spec/lib/sexptitah_spec.rb
+++ spec/lib/sexptitah_spec.rb
@@ -24,6 +24,14 @@ describe 'Sexptitah' do
       end
     end
 
+    describe 'Strings' do
+      it "should correctly render strings" do
+        "foobar".to_sexp.should == '"foobar"'
+        "foo\nbar".to_sexp.should == '"foo\nbar"'
+        "foo\tbar".to_sexp.should == '"foo\tbar"'
+      end
+    end
+
     describe Symbol do
       it "should correctly render symbols" do
         :foo.to_sexp.should == 'foo'
@@ -46,6 +54,11 @@ describe 'Sexptitah' do
       end
       it "should correctly render an improper list" do
         Cons.chain(1,2,3,4).to_sexp.should == '(1 2 3 . 4)'
+      end
+
+      it "should correctly render this list: '(1 2 (3 \"asdf\") foo :bar [1 2 3])'" do
+        Cons.list(1, 2, Cons.list(3, "asdf"), :foo, :bar.keyword, [1, 2, 3]).to_sexp.should ==
+         "(1 2 (3 \"asdf\") foo :bar [1 2 3])"
       end
     end
 
@@ -70,6 +83,87 @@ describe 'Sexptitah' do
       it "should correclty render a hash as an alist" do
         @h.to_sexp.should == '((1 . 2) (foo 1 2 3))'
       end
+    end
+
+    describe 'Boolean' do
+      it "true => true" do
+        true.to_sexp.should == 'true'
+      end
+      it "false => nil" do
+        false.to_sexp.should == 'nil'
+      end
+    end
+  end
+
+  describe "Deserialization" do
+    describe SexptitahParser do
+      def parse(str)
+        SexptitahParser.new(str).parse
+      end
+      it "should correctly parse a number" do
+        parse("12").should == [12]
+      end
+      it "should correctly parse a string" do
+        parse('"foobar\n"').should == ["foobar\n"]
+      end
+      it "should correctly parse a symbol" do
+        parse('foobar').should == [:foobar]
+      end
+      it "should correctly parse nil" do
+        parse("nil").should == [nil]
+      end
+      it "should correctly parse symbols with dashes" do
+        parse("foo-bar").should == [:foo_bar]
+      end
+      it "should correctly parse symbols with spaces" do
+        parse('foo\ bar').should == [:"foo\\ bar"]
+      end
+      it 'should correclty parse a keyword symbol' do
+        parse(':foobar').should == [:':foobar']
+      end
+      it 'should correclty parse a list' do
+        parse('(foo bar 1 "baz")').should == [Cons.list(:foo, :bar, 1, "baz")]
+      end
+      it 'should correctly parse an array' do
+        parse('[foo bar 1 "baz"]').should == [[:foo, :bar, 1, "baz"]]
+      end
+      it 'should correctly parse a nested list' do
+        parse('(foo bar (1 baz) 2)').should == [Cons.list(:foo, :bar, Cons.list(1, :baz), 2)]
+      end
+      it 'should correctly parse nested arrays' do
+        parse('[[1 2][3 4]]').should == [[[1,2],[3,4]]]
+      end
+
+      it 'should correctly parse a mixed array/list' do
+        parse('[(1 2) [3 4] (5 [6 (7)])]').should ==
+          [[Cons.list(1,2), [3,4], Cons.list(5, [6, Cons.list(7)])]]
+      end
+
+      it 'should correctly parse a cons cell' do
+        parse('(1 . 2)').should == [Cons[1, 2]]
+      end
+
+      it 'should correctly handle a dotted proper list' do
+        parse('(1 . (2 3))').should == [Cons.list(1, 2, 3)]
+      end
+
+      it 'should raise an error if more than a sexp follows the dot.' do
+        expect { parse('(1 . 2 3)') }.to raise_error
+      end
+
+      it 'should correctly parse a cons whose cdr is an array.' do
+        parse('(1 . [2 3])').should == [Cons[1, [2,3]]]
+      end
+
+      it 'should correctly parse a cons whose cdr is nil.' do
+        parse('(1 . nil)').should == [Cons[1, nil]]
+      end
+
+      it 'should raise an error if a dot is found outside of a list' do
+        expect { parse('1 . 2') }.to raise_error
+        expect { parse('[1 . 2]') }.to raise_error
+      end
+
     end
   end
 
